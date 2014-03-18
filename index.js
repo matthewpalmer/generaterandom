5c7ae04dcbe55d59811cce73cd6a7382d75d6fdc
michaeld <michaeld@connection-telecom.com>
2014-03-18 09:11:27 +0200

    Make use of length parameter in number()
    The length parameter was added to number(), but it isn't actually used
    (it still uses 2)

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function string(length) {
 function number(length) {
   var text = "";
   var possible = "0123456789";
-  for (var i=0; i < 2; i++)
+  for (var i=0; i < length; i++)
     text += possible.charAt(Math.floor(Math.random() * possible.length));
   return text;
 }
